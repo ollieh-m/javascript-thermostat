53a0236511cd5e3d308aae70e983e6ad4e231355
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-11 16:14:03 +0100

    Thermostat completed and all tests passing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -13,19 +13,71 @@ describe("Thermostat", function() {
 
   it("should increase the temp when up button passed", function() {
     thermostat.upButton()
-    expect(thermostat.temperature).toEqual(21)
+    expect(thermostat.temperature).toEqual(thermostat.RESET_DEFAULT + 1)
   });
 
   it("should decrease the temp when down button passed", function() {
     thermostat.downButton()
-    expect(thermostat.temperature).toEqual(19)
+    expect(thermostat.temperature).toEqual(thermostat.RESET_DEFAULT - 1)
   });
 
   it("should not be able to lower the temp once min temp reached", function() {
     for (var t = 0; t < 12; t++){
       thermostat.downButton()
     }
-    expect(thermostat.temperature).toEqual(10)
+    expect(thermostat.temperature).toEqual(thermostat.MINIMUM_TEMP)
   });
 
+  describe("power saving", function() {
+    it("should have a max temp of 25 degrees by default", function(){
+      for (var t = 0; t < 12; t++){
+        thermostat.upButton()
+      }
+      expect(thermostat.temperature).toEqual(thermostat.PSM_ON_MAX_TEMP)
+    });
+
+    it("should have a max temp of 32 degrees when power save mode is off",function(){
+      thermostat.powerSaveButton()
+      for (var t = 0; t < 15; t++){
+        thermostat.upButton()
+      }
+      expect(thermostat.temperature).toEqual(thermostat.PSM_OFF_MAX_TEMPT)
+    });
+
+    it("should have a max temp of 25 degrees when power save mode is on",function(){
+      thermostat.powerSaveButton()
+      thermostat.powerSaveButton()
+      for (var t = 0; t < 15; t++){
+        thermostat.upButton()
+      }
+      expect(thermostat.temperature).toEqual(thermostat.PSM_ON_MAX_TEMP)
+    });
+  });
+
+  it("should return the temp to 20 degrees when reset button is pressed", function(){
+    thermostat.upButton()
+    thermostat.upButton()
+    thermostat.resetButton()
+    expect(thermostat.temperature).toEqual(thermostat.RESET_DEFAULT)
+  });
+
+  describe("Display status", function() {
+    it("shows low-usage when temp is less than 18 degrees", function() {
+      for (var t = 0; t < 3; t++){
+        thermostat.downButton()
+      }
+      expect(thermostat.displayStatus()).toEqual("low-usage")
+    });
+
+    it("shows med-usage when temp is less than 25 degrees but greater than 17 degrees", function() {
+      expect(thermostat.displayStatus()).toEqual("med-usage")
+    });
+
+    it("shows high-usage when temp is more than 24 degrees", function() {
+      for (var t = 0; t < 7; t++){
+        thermostat.upButton()
+      }
+      expect(thermostat.displayStatus()).toEqual("high-usage")
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,14 +2,45 @@
 
 function Thermostat() {
   this.temperature = 20;
+  this.powerSave = true;
+  this.MINIMUM_TEMP = 10
+  this.PSM_ON_MAX_TEMP = 25
+  this.PSM_OFF_MAX_TEMPT = 32
+  this.RESET_DEFAULT = 20
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 }
 
 Thermostat.prototype.upButton = function() {
-  this.temperature += 1
+  if (this.powerSave === false) {
+    if (this.temperature < this.PSM_OFF_MAX_TEMPT) {
+      this.temperature += 1
+    }
+  } else {
+    if (this.temperature < this.PSM_ON_MAX_TEMP) {
+      this.temperature += 1
+    }
+  }
+}
+Thermostat.prototype.downButton = function() {
+  if (this.temperature > this.MINIMUM_TEMP){
+   this.temperature -= 1
+ }
 }
 
-Thermostat.prototype.downButton = function() {
-  if (this.temperature > 10){
-     this.temperature -= 1
+Thermostat.prototype.powerSaveButton = function() {
+  this.powerSave = !(this.powerSave)
+}
+
+Thermostat.prototype.resetButton = function() {
+  this.temperature = this.RESET_DEFAULT
+}
+
+Thermostat.prototype.displayStatus = function() {
+  if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT){
+    return "low-usage"
+  } else if (this.temperature < this.PSM_ON_MAX_TEMP) {
+    return "med-usage"
+  } else {
+    return "high-usage"
   }
 }
