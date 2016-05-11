e2c16e8ced3108f8e385b43e72adf89026cef287
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-11 14:55:16 +0100

    first 4 bullet points complete - min temp working and all tests paaing

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -8,7 +8,24 @@ describe("Thermostat", function() {
   });
 
   it("should have a starting temperature of 20 degrees", function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.temperature).toEqual(20)
+  });
+
+  it("should increase the temp when up button passed", function() {
+    thermostat.upButton()
+    expect(thermostat.temperature).toEqual(21)
+  });
+
+  it("should decrease the temp when down button passed", function() {
+    thermostat.downButton()
+    expect(thermostat.temperature).toEqual(19)
+  });
+
+  it("should not be able to lower the temp once min temp reached", function() {
+    for (var t = 0; t < 12; t++){
+      thermostat.downButton()
+    }
+    expect(thermostat.temperature).toEqual(10)
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,3 +3,13 @@
 function Thermostat() {
   this.temperature = 20;
 }
+
+Thermostat.prototype.upButton = function() {
+  this.temperature += 1
+}
+
+Thermostat.prototype.downButton = function() {
+  if (this.temperature > 10){
+     this.temperature -= 1
+  }
+}
